8e9864c62f5af1eea8df1df2962aeb06d3d5237f
Volodymyr Gromoglasov <volodymyrgromoglasov@gmail.com>
2019-02-17 16:24:23 +0000

    initial commit and procedural terrain generation, adding noise map and octaves

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidht;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+    public bool autoUpdate;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidht, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap (noiseMap);
+    }
+
+    void onValidate() {
+        if (mapWidht < 1) mapWidht = 1;
+        if (mapHeight < 1) mapHeight = 1;
+        if (lacunarity < 1) lacunarity = 1;
+        if (octaves < 0) octaves = 0;
+
+    }
+}
